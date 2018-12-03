ccf20a9bf47f8efb2a47bdbf0e932c2ceeef33e4
JP <jpluehrig@gmail.com>
2018-12-03 23:31:41 +0100

    Done day 03

---
+++ AdventOfCodeSolvings/Day03.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeSolvings
+{
+    public class Day03 : DayInterface<int, int>
+    {
+        public int RunPartA(List<string> input)
+        {
+            int[,] array = new int[1000, 1000];
+
+            /**
+             -> x
+             # # # # # # # 
+             # # # # # # # 
+             I
+             V Y
+             */
+            foreach (var item in input)
+            {
+                var split = item.Split(' ');
+
+                var id = int.Parse(split[0].Remove(0, 1));
+                /* split[1] = @ */
+                var startSplit = split[2].Replace(":", "").Split(',');
+                int startX = int.Parse(startSplit[0]);
+                int startY = int.Parse(startSplit[1]);
+                var widthSplit = split[3].Split('x');
+                int widthX = int.Parse(widthSplit[0]);
+                int widthY = int.Parse(widthSplit[1]);
+
+                for(var i = startX; i < startX + widthX; i++)
+                {
+                    for(var j = startY; j < startY + widthY; j++)
+                    {
+                        array[i, j]++;
+                    }
+
+                }
+            }
+
+            int minimumTwoOverlaps = 0;
+
+            for (var i = 0; i < 1000; i++)
+            {
+                for (var j = 0; j < 1000; j++)
+                {
+                    if(array[i, j] >= 2)
+                    {
+                        minimumTwoOverlaps++;
+                    }
+                }
+            }
+
+            return minimumTwoOverlaps;
+        }
+
+        public class LineObject
+        {
+            private readonly int[,] array = new int[1000, 1000];
+            public LineObject(int startX, int startY, int widthX, int widthY)
+            {
+
+                for (var i = startX; i < startX + widthX; i++)
+                {
+                    for (var j = startY; j < startY + widthY; j++)
+                    {
+                        array[i, j] = 1;
+                    }
+
+                }
+            }
+
+            public int[,] Array => array;
+        }
+
+        public int RunPartB(List<string> input)
+        {
+            var parsedLineObjects = new Dictionary<int, LineObject>();
+
+            /**
+                -> x
+                # # # # # # # 
+                # # # # # # # 
+                I
+                V Y
+                */
+            foreach (var item in input)
+            {
+                var split = item.Split(' ');
+
+                var id = int.Parse(split[0].Remove(0, 1));
+                /* split[1] = @ */
+                var startSplit = split[2].Replace(":", "").Split(',');
+                int startX = int.Parse(startSplit[0]);
+                int startY = int.Parse(startSplit[1]);
+                var widthSplit = split[3].Split('x');
+                int widthX = int.Parse(widthSplit[0]);
+                int widthY = int.Parse(widthSplit[1]);
+                parsedLineObjects.Add(id, new LineObject(startX, startY, widthX, widthY));
+            }
+
+            foreach(var item in parsedLineObjects)
+            {
+
+            }
+
+            var result = parsedLineObjects.Except(coveredIds);
+
+            if (result.ToArray().Length != 1)
+            {
+                Console.WriteLine("Too much elements containing");
+            }
+            return result.First();
+        }
+    }
+}
---
+++ AdventOfCodeSolvingsTest/Day03Test.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCodeSolvings;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace AdventOfCodeSolvingsTest
+{
+    class Day03Test
+    {
+        [Test]
+        public void RunPartA_TestChain()
+        {
+
+            var sut = new Day03();
+            var result = sut.RunPartA(new List<string>() { "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"});
+
+            Assert.AreEqual(4, result);
+        }
+
+        [Test]
+        public void RunPartA_FileExample()
+        {
+
+            var sut = new Day03();
+            var list = FileLoader.LoadLinesFromFile("../../../../Data/data_day03.txt");
+            var result = sut.RunPartA(list);
+
+            System.Console.WriteLine(result);
+
+            // 1736845 at 17:34 wrong too high
+            // 118840 , wrong day started... Started with day 02... so correct in 23:06
+        }
+
+        [Test]
+        public void RunPartB_RunPartB()
+        {
+
+            var sut = new Day03();
+            var result = sut.RunPartB(new List<string>() { "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2" });
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void RunPartB_RunFromFile()
+        {
+
+            var sut = new Day03();
+            var list = FileLoader.LoadLinesFromFile("../../../../Data/data_day03.txt");
+            var result = sut.RunPartB(list);
+
+            System.Console.WriteLine(result);
+
+            // 919 in additional 16:00 min, but I am not pleased with the allocation of 1.000.000 Elements and therefore a list
+            // more objectorientated: parse the object and look for each object if it intersects with each other
+        }
+    }
+}
