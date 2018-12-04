88c26a86765a4c769b41e50cabdaf3a8ea4659e9
JP <jpluehrig@gmail.com>
2018-12-04 08:20:48 +0100

    Added Day 04 not finished yet

---
+++ AdventOfCodeSolvings/Day04.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCodeSolvings
+{
+    public class Day04 : DayInterface<int, int>
+    {
+        public Dictionary<int, Guard> guardIndex = new Dictionary<int, Guard>();
+        public int RunPartA(List<string> input)
+        {
+            Guard guard = null ;
+            foreach (var guardTimes in input)
+            {
+                var split = guardTimes.Split(']');
+                var time = DateTime.Parse(split[0].Replace("[", ""));
+
+                if (split[1].Contains(" begins shift"))
+                {
+                    var id = int.Parse(split[1].Split(' ')[2].Replace("#", ""));
+                    guard = new Guard(id);
+                    var dat = time.Date;
+                    guard.ActiveMidnightTime.Add(time.Date, new int[60]);
+                    var guardActive = guard.ActiveMidnightTime[time.Date];
+                    for (int i = 0; i < 60; i++)
+                    {
+                        guardActive[i] = 1;
+                    }
+                }
+                else if(split[1].Contains("falls asleep"))
+                {
+                    var guardActive = guard.ActiveMidnightTime[time.Date];
+                    for (int i = time.Minute; i < 60; i++)
+                    {
+                        guardActive[i] = 0;
+                    }
+                }
+                else if (split[1].Contains("wakes up"))
+                {
+                    var guardActive = guard.ActiveMidnightTime[time.Date];
+
+                    for (int i = time.Minute; i < 60; i++)
+                    {
+                        guardActive[i] = 1;
+                    }
+                }
+            }
+
+            int idGuard = 0;
+            int maxTimeAsleep = 0;
+            int bestMinute = 0;
+            int[] globalOverlap = new int[60];
+            foreach (var guarding in guardIndex)
+            {
+                int sum = 0;
+                int[] overlap = new int[60];
+                foreach (var timeslot in guarding.Value.ActiveMidnightTime.Values)
+                {
+                    overlap = overlap.Intersect(timeslot).ToArray();
+                    sum += timeslot.Sum();
+                }
+                if (sum > maxTimeAsleep)
+                {
+                    maxTimeAsleep = sum;
+                    idGuard = guarding.Key;
+                    bestMinute = overlap.Max();
+                }
+            }
+            
+            return idGuard * bestMinute;
+        }
+
+        
+
+        public int RunPartB(List<string> input)
+        {
+
+            return -1;
+        }
+
+        public class Guard
+        {
+            public Dictionary<DateTime, int[]> ActiveMidnightTime = new Dictionary<DateTime, int[]>();
+
+            public int Id = -1;
+            public Guard(int id)
+            {
+                Id = id;
+            }
+        }
+    }
+}
---
+++ AdventOfCodeSolvingsTest/Day04Test.cs
@@ -0,0 +1,73 @@
+﻿using AdventOfCodeSolvings;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System;
+
+namespace AdventOfCodeSolvingsTest
+{
+    class Day04Test
+    {
+        [Test]
+        public void RunPartA_TestChain()
+        {
+
+            var sut = new Day04();
+            var list = FileLoader.LoadLinesFromFile("../../../../Data/data_day04_test.txt");
+            var result = sut.RunPartA(list);
+
+            Assert.AreEqual(240, result);
+        }
+
+        [Test]
+        public void RunPartA_FileExample()
+        {
+
+            var sut = new Day04();
+            var list = FileLoader.LoadLinesFromFile("../../../../Data/data_day04.txt");
+            var result = sut.RunPartA(list);
+
+            System.Console.WriteLine(result);
+
+            // 1736845 at 17:34 wrong too high
+            // 118840 , wrong day started... Started with day 02... so correct in 23:06
+        }
+
+        [Test]
+        public void RunPartB_RunPartB()
+        {
+
+            var sut = new Day04();
+            var result = sut.RunPartB(new List<string>() { "#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2" });
+
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void RunPartB_RunFromFile()
+        {
+
+            var sut = new Day04();
+            var list = FileLoader.LoadLinesFromFile("../../../../Data/data_day03.txt");
+            var result = sut.RunPartB(list);
+
+            System.Console.WriteLine(result);
+
+            // 919 in additional 16:00 min, but I am not pleased with the allocation of 1.000.000 Elements and therefore a list
+            // more objectorientated: parse the object and look for each object if it intersects with each other
+        }
+
+        [Test]
+        public void DateTimeParseTest()
+        {
+            var date = DateTime.Parse("1518-11-01 05:26");
+
+            Assert.AreEqual(1518, date.Year);
+            Assert.AreEqual(11, date.Month);
+            Assert.AreEqual(01, date.Day);
+            Assert.AreEqual(5, date.Hour);
+            Assert.AreEqual(26, date.Minute);
+        }
+    }
+}
